7bdb43ce73e01c3e43f3daf4d676172aa2360fb9
srpgrl <60191273+srpgrl@users.noreply.github.com>
2020-02-17 00:19:43 +0300

    Add files via upload

---
+++ Pythagorean Triangles (1,100).py
@@ -0,0 +1,11 @@
+mylist=[]
+def pytha():
+    for i in range(1,101):
+        for j in range (1,101):
+            c == (i**2 + (j**2)) ** 0.5
+            if type(c) == int:
+                mylist.append((i,j,c))
+
+print(mylist)
+
+
